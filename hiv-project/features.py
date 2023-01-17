21b6525de542b201ff726c3190d4198ab411b618
Katarina Vukosavljevic <v-katarina@hotmail.com>
2023-01-17 11:51:50 +0000

    Added updated data

--- hiv-project/features.py
+++ hiv-project/features.py
@@ -1,6 +1,10 @@
 import numpy as np
 import neurokit2 as nk
 import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.signal import argrelextrema
+from itertools import pairwise
+from sklearn.metrics import auc
 
 
 def ecg_features(data):
@@ -25,9 +29,8 @@ def ecg_features(data):
     hrvs.dropna(axis='columns', inplace=True)
     return hrvs.to_numpy(dtype=float)
 
-def get_features(data, columns):
+def get_features_ecg(data, columns):
 
-    
     ecg_cols = list([True if ('ECG' in name) else False for name in columns])
 
     ecg_data = np.array(data[:,ecg_cols])
@@ -47,3 +50,87 @@ def get_features(data, columns):
         hrvs.append(ecg_feats)
 
     return hrvs
+
+def get_features_ppg(data, columns):
+    "Retrieving features from ppg data"
+
+    plot_ppg = False
+    if plot_ppg is True: plotppg(data, columns)
+
+    # What do SPO2 status jumps mean?
+
+    ############# PPG data ##################
+    # Extracting features mentioned in https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6426305/
+
+    # There are 4 ppg and 7 ppg second derivative features
+    # ppg_features = [Systolic Amplitude, Pulse Area, Peak to Peak Interval, Large Artery Stiffness Index]
+    ppg_feats = ['sys_amp', 'pulse_area', 'p2p_int', 'las_index']
+    ppg_features = {}
+
+    # for sys_amp detect local maxima
+    # for pulse area, detect local minima and integrate under 
+    # for p2p interval, detect differences between local maxima in value
+    # for las_index p2p time interval
+
+    loc_max = argrelextrema(data[:,4], np.greater)[0]
+    loc_min = argrelextrema(data[:,4], np.less)[0]
+
+    ppg_features["sys_time"] = data[loc_max,0]
+    ppg_features["sys_amp"] = data[loc_max,4]
+
+    ppg_features["dia_time"] = data[loc_min,0]
+    ppg_features["dia_amp"] = data[loc_min,4]
+
+    #
+    # ppg_features["pulse_area"] = auc(xx,yy)
+
+    ppg_features["p2p_int"] = np.array([y - x for x, y in pairwise(ppg_features["sys_amp"])])
+    print('------------------')
+    
+    print(ppg_features['sys_amp'])
+    print(ppg_features['p2p_int'])
+
+    print('-------------------')
+
+    ppg_features["las_ind"] = np.array([y - x for x, y in pairwise(data[loc_max,0])])
+    
+    plt.plot(data[:,0], data[:,4])
+    plt.plot(data[loc_max,0], data[loc_max,4], 'r*')
+    plt.plot(data[loc_min,0], data[loc_min,4], 'r*')
+    plt.show()
+
+    plt.hist(ppg_features["p2p_int"])
+    plt.title('Peak-to-peak lengths in ms')
+    plt.show()
+
+    plt.hist(ppg_features["las_ind"])
+    plt.show()
+
+    input()
+
+    return data
+
+def plotppg(data, columns):
+    print(columns)
+
+    fig, axs = plt.subplots(3, 2)
+    axs[0, 0].plot(data[:,0], data[:,1])
+    axs[0, 1].plot(data[:,0], data[:,2], 'tab:orange')
+    axs[1, 0].plot(data[:,0], data[:,3], 'tab:green')
+    axs[1, 1].plot(data[:,0], data[:,5], 'tab:red')
+    axs[2, 0].plot(data[:,0], data[:,4], 'tab:blue')
+    axs[2, 1].plot(data[:int(len(data)/8),0], data[:int(len(data)/8),4], 'tab:blue')
+
+    axs[0, 0].set_ylabel(columns[1])
+    axs[0, 1].set_ylabel(columns[2])
+    axs[1, 0].set_ylabel(columns[3])
+    axs[1, 1].set_ylabel(columns[5])
+    axs[2, 0].set_ylabel(columns[4])
+    axs[2, 1].set_ylabel('Zoomed in ' + columns[4])
+    
+
+    for ax in axs.flat:
+        ax.set(xlabel=columns[0])
+
+    fig.show()
+    input()
--- hiv-project/main.py
+++ hiv-project/main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 from data import get_file_names, get_datatype, read_data, get_label
-from features import get_features
+from features import get_features_ecg, get_features_ppg
 
 from pathlib import Path 
 import typer
@@ -53,7 +53,7 @@ def main(
 
             # Obtain features
             try:
-                features = get_features(data, columns)
+                features = get_features_ecg(data, columns)
 
                 if label == 0 and visit == 1:
                     if np.array(features).shape[2] == 25:
@@ -93,13 +93,13 @@ def main(
         print('Data type is ppg.')
 
         # Data prep
-        tr_normal_ppg = []
-        tr_abnormal_ppg = []
+        tr_low_ppg = []
+        tr_high_ppg = []
 
-        ts_normal_ppg = []
-        ts_abnormal_ppg = []
+        ts_low_ppg = []
+        ts_high_ppg = []
 
-        for file in file_names[:1]:
+        for file in file_names[:3]:
             print(f'Reading the {datatype} data.')
 
             # Get label
@@ -113,45 +113,40 @@ def main(
             # Get the data
             data, columns = read_data(file,datatype)
 
-            break
-
             # Obtain features
+            features = get_features_ppg(data, columns)
             try:
-                features = get_features(data, columns)
+                features = get_features_ppg(data, columns)
 
                 if label == 0 and visit == 1:
-                    if np.array(features).shape[2] == 25:
-                        tr_normal_ppg.extend(features)
+                    tr_low_ppg.extend(features)
                 elif label == 1 and visit == 1:
-                    if np.array(features).shape[2] == 25:
-                        tr_abnormal_ppg.extend(features)
+                    tr_high_ppg.extend(features)
                 elif label == 0 and visit == 2:
-                    if np.array(features).shape[2] == 25:
-                        ts_normal_ppg.extend(features)
+                    ts_low_ppg.extend(features)
                 elif label == 1 and visit == 2:
-                    if np.array(features).shape[2] == 25:
-                        ts_abnormal_ppg.extend(features)
+                    ts_high_ppg.extend(features)
                 else:
                     print(f"Not categorised for label {label} and visit {visit}.")
             except:
                 print(f"Not able to obtain features for file: {file}")
 
-        tr_normal_ppg = np.array(tr_normal_ppg)
-        tr_abnormal_ppg = np.array(tr_abnormal_ppg)
+        tr_low_ppg = np.array(tr_low_ppg)
+        tr_high_ppg = np.array(tr_high_ppg)
 
-        ts_normal_ppg = np.array(ts_normal_ppg)
-        ts_abnormal_ppg = np.array(ts_abnormal_ppg)
+        ts_low_ppg = np.array(ts_low_ppg)
+        ts_high_ppg = np.array(ts_high_ppg)
         
         # Data shapes
-        print(f"Training data shapes: normal {tr_normal.shape} and abnormal {tr_abnormal.shape}.")
-        print(f"Test data shapes: normal {ts_normal.shape} and abnormal {ts_abnormal.shape}.")
+        print(f"Training data shapes: low risk {tr_low_ppg.shape} and high risk {tr_high_ppg.shape}.")
+        print(f"Test data shapes: low risk {ts_low_ppg.shape} and high risk {ts_high_ppg.shape}.")
 
         # Saving the data
-        np.save('tr_normal_ppg', tr_normal_ppg)
-        np.save('tr_abnormal_ppg', tr_abnormal_ppg)
+        np.save('tr_low_ppg', tr_low_ppg)
+        np.save('tr_high_ppg', tr_high_ppg)
 
-        np.save('ts_normal_ppg', ts_normal_ppg)
-        np.save('ts_abnormal_ppg', ts_abnormal_ppg)
+        np.save('ts_low_ppg', ts_low_ppg)
+        np.save('ts_high_ppg', ts_high_ppg)
 
     else: 
         print(f'Dataype {datatype} is not a recognised datatype.')
